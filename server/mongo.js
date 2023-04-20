6f697f8f141a3671ac7a1ea56ab75927a2cddbbc
Carlos Macario <cmacariovx@gmail.com>
2023-04-20 16:20:36 -0400

    small

--- server/mongo.js
+++ server/mongo.js
@@ -1,6 +1,6 @@
 const { Pool } = require('pg');
 require('dotenv').config()
-const connectionString = process.env.POSTGRES_URL;
+const connectionString = process.env.DATABASE_URL;
 const pool = new Pool({
     connectionString,
     ssl: {
