0801aec0b7209f0f67f93be1b2c9578a4eb57678
Carlos Macario <cmacariovx@gmail.com>
2023-04-20 16:10:21 -0400

    small

--- server/mongo.js
+++ server/mongo.js
@@ -1,5 +1,5 @@
 const { Pool } = require('pg');
-const connectionString = process.env.DATABASE_URL;
+const connectionString = process.env.POSTGRES_URL;
 const pool = new Pool({ connectionString });
 
 async function fetchBrain(req, res, next, newNeuralNetwork) {
